26df063ec98a055155830828723bcb05cfb3ed0d
sahishnusuresh <sahishnusuresh@gmail.com>
2023-02-03 17:51:30 +0530

    select any time

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,11 +9,37 @@ test('book a ticket',async({browser}:{browser:Browser}):Promise<void>=>{
   await page.getByRole('img', { name: 'Pathaan' }).click();
   await page.getByRole('button', { name: 'Book tickets' }).click();
   await page.getByText('2D',{exact: true}).nth(2).click()
+  // const times=await page.$$eval('a',(elements:HTMLInputElement)=>{
+  //   return elements.filter(element=>{})
+  // }
+    
+//   
+// var arr :Array<HTMLLinkElement>|Array<HTMLAnchorElement>= [], l = window.document.links;
+// for(var i=0; i<l.length; i++) {
+//   if(l[i].innerHTML=='04:30 PM'){
+//     arr.push(l[i].href)
+//   }
+// }
+// await page.click(arr[0])
+// await page.$$eval('a.showtime-pill',links=>
+//       links.forEach(async(link)=>{
+//         if(link instanceof HTMLLinkElement && link.className=='__text' && link.textContent=='04:30 PM'){
+//           await link.click()
+//         }
+//       }
 
-  await page.getByRole('link',{name:'1:30 PM'}).click()
+//       ))
+await page.getByRole('link').filter({hasText:'04:30 PM'}).click()
+
+
+
+  
+  //  await page.getByRole('link',{name:'1:30 PM'}).click() 
   await page.locator('#btnPopupAccept').click()
   await page.locator('#pop_1').click()
   await page.getByText('Select Seats').click()
+  const seatsleft=await page.locator('._available').count()
+  const selectfrom=seatsleft/2
   await page.locator('#A_8_11').getByText('9').click()
   await page.getByRole('link',{name:'Pay Rs.150.00'}).click()
 
