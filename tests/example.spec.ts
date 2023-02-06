b4f3d9fd3c31ae55cc441969faf43c56a63b4067
sahishnusuresh <sahishnusuresh@gmail.com>
2023-02-06 16:15:56 +0530

    tried links

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,21 +14,21 @@ test('book a ticket',async({browser}:{browser:Browser}):Promise<void>=>{
   // }
     
 //   
-// var arr :Array<HTMLLinkElement>|Array<HTMLAnchorElement>= [], l = window.document.links;
-// for(var i=0; i<l.length; i++) {
-//   if(l[i].innerHTML=='04:30 PM'){
-//     arr.push(l[i].href)
-//   }
-// }
-// await page.click(arr[0])
-// await page.$$eval('a.showtime-pill',links=>
-//       links.forEach(async(link)=>{
-//         if(link instanceof HTMLLinkElement && link.className=='__text' && link.textContent=='04:30 PM'){
-//           await link.click()
-//         }
-//       }
+var arr :Array<HTMLLinkElement>|Array<HTMLAnchorElement>= [], l = window.document.links;
+for(var i=0; i<l.length; i++) {
+  if(l[i].innerHTML=='04:30 PM'){
+    arr.push(l[i].href)
+  }
+}
+await page.click(arr[0])
+await page.$$eval('a.showtime-pill',links=>
+      links.forEach(async(link)=>{
+        if(link instanceof HTMLLinkElement && link.className=='__text' && link.textContent=='04:30 PM'){
+          await link.click()
+        }
+      }
 
-//       ))
+      ))
 await page.getByRole('link').filter({hasText:'04:30 PM'}).click()
 
 
@@ -38,19 +38,7 @@ await page.getByRole('link').filter({hasText:'04:30 PM'}).click()
   await page.locator('#btnPopupAccept').click()
   await page.locator('#pop_2').click()
   await page.getByText('Select Seats').click()
-  await page.$$eval('._available', (seats)=>{
-    console.log('hi')
-    let count=0;
-    seats.forEach((seat)=>{
-      while(count<=2){
-      if(seat.parentElement?.hasAttribute('id=A_1_01')){
-         page.locator('#A_8_11').getByText('9').click()
-        count+=1;
-      }
-    }
-      // await page.click(seat)
-    })})
-  var str="#[A-Z]_1_[10-20]"
+  
 
 const seats= page.locator('#A_1_01 > a._available')
 const no_of_seats=await seats.count() // to get number of available seats
